82461b9ecef6e02e8ddde2dc2e4e6ea2cb22faac
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2024-12-26 22:11:22 +0800

    gitignore

--- src/main.jsx
+++ src/main.jsx
@@ -6,8 +6,8 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
 // Initialize the supabase client
 const supabase = createClient(
-  "https://kcvranbgzvrodkibapem.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjdnJhbmJnenZyb2RraWJhcGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjAwODksImV4cCI6MjA1MDQ5NjA4OX0.mSekUwD18zeKesfxSpFv4SmtfMR3szHNsPGKRUQjK2k"
+  import.meta.env.VITE_SUPABASE_URL,
+  import.meta.env.VITE_SUPABASE_KEY
 );
 
 createRoot(document.getElementById("root")).render(
