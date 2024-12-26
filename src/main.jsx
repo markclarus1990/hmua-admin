687ed7d45d8aadeb0520213939987f36545df121
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2024-12-26 19:41:05 +0800

    updated auth

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { SessionContextProvider } from "@supabase/auth-helpers-react";
 // Initialize the supabase client
 const supabase = createClient(
   "https://kcvranbgzvrodkibapem.supabase.co",
-  "your-supabase-key"
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtjdnJhbmJnenZyb2RraWJhcGVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjAwODksImV4cCI6MjA1MDQ5NjA4OX0.mSekUwD18zeKesfxSpFv4SmtfMR3szHNsPGKRUQjK2k"
 );
 
 createRoot(document.getElementById("root")).render(
