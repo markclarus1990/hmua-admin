cd6b61c24383f13ee47595ef93e4c9d845ffedcf
markclarus1990 <43287199+markclarus1990@users.noreply.github.com>
2024-12-26 21:47:16 +0800

    change the base

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/hmua-admin/", // Set the base path for GitHub Pages
+  base: "https://markclarus1990.github.io/hmua-admin/", // Set the base path for GitHub Pages
 });
